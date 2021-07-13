cdc5d787af5e0d7939fcbf4127dc694d445fd2bc
Nishit <84226173+baburao-2708@users.noreply.github.com>
2021-07-13 16:36:31 +0530

    Add files via upload

---
+++ THAs/Day-16 A/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="App">
+      <div class="container">
+        <div class="main1">
+          <img
+            src="https://res.cloudinary.com/purnesh/image/upload/f_auto/v1609312735/21609312734288.jpg"
+            alt=""
+          />
+        </div>
+        <div className="main2">
+          <h2>Meme Card</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
