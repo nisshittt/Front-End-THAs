f1cc6ae6767081598c5aace25bd7ec196fdbf9c3
Nishit <84226173+baburao-2708@users.noreply.github.com>
2021-07-31 13:11:32 +0530

    Day-23 uploaded :)

---
+++ Day-23/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Main from "./components/Main";
+// import Navbar from "./Components/Day 24 Route/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
